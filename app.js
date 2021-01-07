bc8c85a0a330a87d20dee137c6f695eff8a0cac1
Saurab Prasher <saurab015@gmail.com>
2021-01-07 17:01:44 +0530

    hashing func added

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
 const User = require("./models/user");
+const bcrypt = require("bcrypt");
+
+mongoose
+  .connect("mongodb://localhost:27017/authDEMO", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MONGO CONNECTION OPEN!!!");
+  })
+  .catch((err) => {
+    console.log("OH NO MONGO CONNECTION ERROR!!!!");
+    console.log(err);
+  });
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("This is home page");
+});
+
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  const hash = await bcrypt.hash(password, 12);
+  const user = new User({
+    username,
+    password: hash,
+  });
+  await user.save();
+  res.redirect("/");
 });
 
 app.get("/secret", (req, res) => {
