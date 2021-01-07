54a82e83d987167bd880e06aa8e71ad340da4bf7
Saurab Prasher <saurab015@gmail.com>
2021-01-07 20:09:13 +0530

    refactoring

--- app.js
+++ app.js
@@ -27,6 +27,13 @@ app.use(
   session({ secret: "notagoodsecret", resave: false, saveUninitialized: false })
 );
 
+const requireLogin = (req, res, next) => {
+  if (!req.session.user_id) {
+    return res.redirect("/login");
+  }
+  next();
+};
+
 app.get("/", (req, res) => {
   res.send("This is home page");
 });
@@ -37,11 +44,8 @@ app.get("/register", (req, res) => {
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
-  const hash = await bcrypt.hash(password, 12);
-  const user = new User({
-    username,
-    password: hash,
-  });
+
+  const user = new User({ username, password });
   await user.save();
   req.session.user_id = user._id;
   res.redirect("/");
@@ -54,12 +58,10 @@ app.get("/login", (req, res) => {
 app.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
-    const user = await User.findOne({ username: username });
+    const foundUser = await User.findAndValidate(username, password);
 
-    const validPassword = await bcrypt.compare(password, user.password);
-
-    if (validPassword) {
-      req.session.user_id = user._id;
+    if (foundUser) {
+      req.session.user_id = foundUser._id;
       res.redirect("/secret");
     } else {
       res.redirect("/login");
@@ -75,10 +77,7 @@ app.post("/logout", (req, res) => {
   res.redirect("/login");
 });
 
-app.get("/secret", (req, res) => {
-  if (!req.session.user_id) {
-    return res.redirect("/login");
-  }
+app.get("/secret", requireLogin, (req, res) => {
   res.render("secret");
 });
 
